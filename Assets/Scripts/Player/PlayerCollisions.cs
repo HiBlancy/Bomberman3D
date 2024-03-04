19ac9e822950353640feca3cbb9d105a4a03fcf4
Blancy <ievarituma9877@gmail.com>
2024-03-04 22:44:30 +0100

    Adding mixer

---
+++ Assets/Scripts/Player/PlayerCollisions.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    [SerializeField] AudioSource audioClip;
+
+    private void Awake()
+    {
+        audioClip = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Explotion"))
+        {
+            Debug.Log("jugador");
+            //porque detecta multiples veces
+        }
+        if (collision.CompareTag("powerup"))
+        {
+            audioClip.Play();
+        }
+            
+    }
+}
